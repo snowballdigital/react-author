6fd1a693aefaf28cbaea1afe9029c265fc719847
Håkon Krogh <hakonhk@gmail.com>
2019-01-07 09:41:04 +0100

    Added Robert and Michael

--- src/index.js
+++ src/index.js
@@ -1,102 +1,114 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react'
+import styled from 'styled-components'
 
-import ObfuscatedLink from "./obfuscated-link";
+import ObfuscatedLink from './obfuscated-link'
 
-import bardImg from "./team-thumbs/bard.jpg";
-import didrikImg from "./team-thumbs/didrik.jpg";
-import hakonImg from "./team-thumbs/hakon.jpg";
-import stavrosImg from "./team-thumbs/stavros.jpg";
-import bartoszImg from "./team-thumbs/bartosz.jpg";
-import audunImg from "./team-thumbs/audun.jpg";
-import julianImg from "./team-thumbs/julian.jpg";
-import leneImg from "./team-thumbs/lene.jpg";
-import ainaImg from "./team-thumbs/aina.jpg";
-import eduardoImg from "./team-thumbs/eduardo.jpg";
-import filipaImg from "./team-thumbs/filipa.jpg";
-import marieImg from "./team-thumbs/marie.jpg";
-import pawelImg from "./team-thumbs/pawel.jpg";
+import bardImg from './team-thumbs/bard.jpg'
+import didrikImg from './team-thumbs/didrik.jpg'
+import hakonImg from './team-thumbs/hakon.jpg'
+import stavrosImg from './team-thumbs/stavros.jpg'
+import bartoszImg from './team-thumbs/bartosz.jpg'
+import audunImg from './team-thumbs/audun.jpg'
+import julianImg from './team-thumbs/julian.jpg'
+import leneImg from './team-thumbs/lene.jpg'
+import ainaImg from './team-thumbs/aina.jpg'
+import eduardoImg from './team-thumbs/eduardo.jpg'
+import filipaImg from './team-thumbs/filipa.jpg'
+import marieImg from './team-thumbs/marie.jpg'
+import pawelImg from './team-thumbs/pawel.jpg'
+import robertImg from './team-thumbs/robert.jpg'
+import michaelImg from './team-thumbs/michael.jpg'
 
 export const authors = {
-  "bard@snowball.digital": {
-    name: "Bård Farstad",
+  'bard@snowball.digital': {
+    name: 'Bård Farstad',
     image: bardImg,
     links: {
-      twitter: "bardfarstad",
-      linkedin: "bfarstad"
+      twitter: 'bardfarstad',
+      linkedin: 'bfarstad'
     }
   },
-  "didrik@snowball.digital": {
-    name: "Didrik Steen Hegna",
+  'didrik@snowball.digital': {
+    name: 'Didrik Steen Hegna',
     image: didrikImg
   },
-  "stavros@snowball.digital": {
-    name: "Stavros Sotiropoulos",
+  'stavros@snowball.digital': {
+    name: 'Stavros Sotiropoulos',
     image: stavrosImg,
     links: {
-      linkedin: "sta-sotiro"
+      linkedin: 'sta-sotiro'
     }
   },
-  "bartosz@snowball.digital": {
-    name: "Bartosz Sokolow",
+  'bartosz@snowball.digital': {
+    name: 'Bartosz Sokolow',
     image: bartoszImg
   },
-  "audun@snowball.digital": {
-    name: "Audun Ask Blaker",
+  'audun@snowball.digital': {
+    name: 'Audun Ask Blaker',
     image: audunImg
   },
-  "julian@snowball.digital": {
-    name: "Julian Rubilar",
+  'julian@snowball.digital': {
+    name: 'Julian Rubilar',
     image: julianImg
   },
-  "hakon@snowball.digital": {
-    name: "Håkon Gullord Krogh",
+  'hakon@snowball.digital': {
+    name: 'Håkon Gullord Krogh',
     image: hakonImg,
     links: {
-      twitter: "hakonkrogh",
-      linkedin: "hakongullordkrogh"
+      twitter: 'hakonkrogh',
+      linkedin: 'hakongullordkrogh'
     }
   },
-  "lene@snowball.digital": {
-    name: "Lene Aspen Nilsen",
+  'lene@snowball.digital': {
+    name: 'Lene Aspen Nilsen',
     image: leneImg
   },
-  "aina@snowball.digital": {
-    name: "Aina Solvang Lindblom",
+  'aina@snowball.digital': {
+    name: 'Aina Solvang Lindblom',
     image: ainaImg
   },
-  "eduardo@snowball.digital": {
-    name: "Eduardo Morán Alvarado",
+  'eduardo@snowball.digital': {
+    name: 'Eduardo Morán Alvarado',
     image: eduardoImg
   },
-  "filipa@snowball.digital": {
-    name: "Filipa Amado",
+  'filipa@snowball.digital': {
+    name: 'Filipa Amado',
     image: filipaImg,
     links: {
-      linkedin: "filipa-amado",
-      twitter: "filipa_amado"
+      linkedin: 'filipa-amado',
+      twitter: 'filipa_amado'
     }
   },
-  "marie.weidlich@snowball.digital": {
-    name: "Maria Teresa Weidlich",
+  'marie.weidlich@snowball.digital': {
+    name: 'Maria Teresa Weidlich',
     image: marieImg,
     links: {
-      linkedin: "marieweidlich",
-      instagram: "londonfashiontoday"
+      linkedin: 'marieweidlich',
+      instagram: 'londonfashiontoday'
     }
   },
-  "pawel@snowball.digital": {
-    name: "Paweł Kawczyński",
+  'pawel@snowball.digital': {
+    name: 'Paweł Kawczyński',
     image: pawelImg,
     links: {
-      instagram: "kavvvva",
-      linkedin: "pawelkawczynski"
+      instagram: 'kavvvva',
+      linkedin: 'pawelkawczynski'
     }
+  },
+  'robert@snowball.digital': {
+    name: 'Robert Mulcahy',
+    image: robertImg,
+    links: {}
+  },
+  'michael@snowball.digital': {
+    name: 'Michael Smesnik',
+    image: michaelImg,
+    links: {}
   }
-};
+}
 
 const Outer = styled.div.attrs({
-  className: "snowball-author"
+  className: 'snowball-author'
 })`
   display: flex;
   align-items: center;
@@ -109,72 +121,72 @@ const Outer = styled.div.attrs({
       text-decoration: underline;
     }
   }
-`;
+`
 
 const Image = styled.img.attrs({
-  className: "snowball-author__image"
+  className: 'snowball-author__image'
 })`
   border-radius: 50%;
   object-fit: fill;
   height: 25px;
   width: 25px;
   margin-right: 15px;
-`;
+`
 
 const Links = styled.span.attrs({
-  className: "snowball-author__links"
+  className: 'snowball-author__links'
 })`
   font-size: 0.75em;
   margin-left: 10px;
-`;
+`
 
 const Link = styled.a.attrs({
-  className: "snowball-author__link"
+  className: 'snowball-author__link'
 })`
   &:not(:first-child) {
     margin-left: 5px;
   }
-`;
+`
 
 export default class Author extends React.PureComponent {
   renderLink = type => {
-    const { email } = this.props;
-    const data = authors[email];
-    const linkData = data.links[type];
+    const { email } = this.props
+    const data = authors[email]
+    const linkData = data.links[type]
 
     switch (type) {
-      case "twitter":
+      case 'twitter':
         return (
           <Link key={type} href={`https://twitter.com/${linkData}`}>
             Twitter
           </Link>
-        );
-      case "linkedin":
+        )
+      case 'linkedin':
         return (
           <Link key={type} href={`https://www.linkedin.com/in/${linkData}`}>
             LinkedIn
           </Link>
-        );
-      case "instagram":
+        )
+      case 'instagram':
         return (
           <Link key={type} href={`https://www.instagram.com/${linkData}`}>
             Instagram
           </Link>
-        );
+        )
       default:
-        return null;
+        return null
     }
-  };
+  }
 
-  render() {
-    const { email } = this.props;
-    const data = authors[email];
+  render () {
+    const { email } = this.props
+    const data = authors[email]
 
     if (!data) {
-      return null;
+      return null
     }
 
-    const links = Object.keys(data.links || {});
+    const links = Object.keys(data.links || {})
 
     return (
       <Outer>
@@ -182,6 +194,6 @@ export default class Author extends React.PureComponent {
         <ObfuscatedLink href={`mailto:${email}`}>{data.name}</ObfuscatedLink>
         {links.length > 0 && <Links>{links.map(this.renderLink)}</Links>}
       </Outer>
-    );
+    )
   }
 }
--- src/obfuscated-link.js
+++ src/obfuscated-link.js
@@ -1,30 +1,35 @@
-import React from "react";
-import isCallable from "is-callable";
+import React from 'react'
+import isCallable from 'is-callable'
 
 export default class ObfuscatedLink extends React.PureComponent {
-  state = {};
+  state = {}
 
-  componentDidMount() {
-    const { href } = this.props;
+  /**
+   * By setting the href after the page is mounted, we stop
+   * any crawler that pick up email addresses and don't
+   * execute JS
+   */
+  componentDidMount () {
+    const { href } = this.props
 
     if (href) {
       this.setState({
         href
-      });
+      })
     }
   }
 
-  render() {
-    const { children, href, ...rest } = this.props;
+  render () {
+    const { children, href, ...rest } = this.props
     const renderProps = {
       ...rest,
       ...this.state
-    };
-
-    if (isCallable(children)) {
-      return children(renderProps);
     }
 
-    return <a {...renderProps}>{children}</a>;
+    if (isCallable(children)) {
+      return children(renderProps)
+    }
+
+    return <a {...renderProps}>{children}</a>
   }
 }
